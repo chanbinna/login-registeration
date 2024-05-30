4f4a3f67d03b933b8ad1bfbdfbe94605cc111f89
Chanbin Na <chanbinna@gmail.com>
2024-05-30 15:07:14 -0700

    full login feature

---
+++ client/src/app/home/page.js
@@ -0,0 +1,18 @@
+'use client'
+import styles from "../page.module.css";
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+// import Signup from "./Signup";
+
+function home() {
+    return (
+        <main className={styles.main}>
+            <div>
+                <h1>This is the Home Page</h1>
+            </div>
+        </main>
+    );
+}
+
+export default home; 
